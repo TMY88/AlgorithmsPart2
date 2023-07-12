2a1af3ebd9644faf273ecf2621a1eca5cbdc8a0f
Михаил <m-toka@mail.ru>
2023-07-12 21:02:44 +0300

    Initial commit

---
+++ src/main/java/pro/sky/algorithmspart2/AlgorithmsPart2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.algorithmspart2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsPart2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmsPart2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/algorithmspart2/AlgorithmsPart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.algorithmspart2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsPart2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
